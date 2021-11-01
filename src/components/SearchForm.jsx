f85c709151f11ed6a4f894a87d77be87086acca2
albiCoder <58115200+albiCoder@users.noreply.github.com>
2021-11-01 17:25:12 +0100

    don't reset subreddit when clicking search link

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { useParams } from 'react-router';
+import { useParams, useHistory } from 'react-router';
 
 import * as S from './Styled.components';
 
@@ -21,21 +21,31 @@ display: block;
     font-size: 1.5rem;
     margin: 0 1rem;
 `;
+const SearchButton = styled.button`
+font-size: 1.4rem;
+background-color: #FDB755;
+border-radius: .5rem;
+color: #f4f4f4;
+border: solid .2rem #fdb755;
+padding: .9rem 1.5rem .7rem 1.5rem;
+font-weight: 500;
+cursor: pointer;
+position: relative;
+text-transform: uppercase;
+`;
 
 const SearchForm = () => {
   const params = useParams();
+  const history = useHistory();
   const [subreddit, setSubreddit] = useState(params.subreddit);
 
-  function handleSubmit() {
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push(`/search/${subreddit}`);
   }
   function handleChange(event) {
     setSubreddit(event.target.value);
   }
-
-  useEffect(() => {
-    setSubreddit(params.subreddit);
-  }, [params.subreddit]);
 
   return (
     <div>
@@ -47,9 +57,9 @@ const SearchForm = () => {
           /r
         </InputLabel>
         <InputText type="text" value={subreddit} onChange={handleChange} />
-        <S.SearchButton to={`/search/${subreddit}`} onClick={handleSubmit}>
+        <SearchButton onClick={handleSubmit}>
           Search
-        </S.SearchButton>
+        </SearchButton>
       </Form>
     </div>
   );
