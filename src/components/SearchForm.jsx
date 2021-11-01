05770fd40bcb38ed0e3b3d5bf0fd999ddaca8036
albiCoder <58115200+albiCoder@users.noreply.github.com>
2021-11-01 21:42:15 +0100

    api data formated for the heat map

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { useParams, useHistory } from 'react-router';
+import PropType from 'prop-types';
 
 import * as S from './Styled.components';
 
@@ -34,17 +35,261 @@ position: relative;
 text-transform: uppercase;
 `;
 
-const SearchForm = () => {
+const SearchForm = ({ setSearchData }) => {
   const params = useParams();
   const history = useHistory();
   const [subreddit, setSubreddit] = useState(params.subreddit);
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  function handleChange(event) {
+    setSubreddit(event.target.value);
+  }
+
+  function getTime(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    let formatedHour = '';
+    if (hours === 0) {
+      formatedHour += '12:';
+      formatedHour += minutes < 10 ? `0${minutes} am` : `${minutes} am`;
+    } else if (hours > 0 && hours < 12) {
+      formatedHour += `${hours}:`;
+      formatedHour += minutes < 10 ? `0${minutes} am` : `${minutes} am`;
+    } else if (hours === 12) {
+      formatedHour += '12:';
+      formatedHour += minutes < 10 ? `0${minutes} pm` : `${minutes} pm`;
+    } else {
+      formatedHour += `${hours - 12}:`;
+      formatedHour += minutes < 10 ? `0${minutes} pm` : `${minutes} pm`;
+    }
+    return formatedHour;
+  }
+  function getDay(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const day = date.getDay();
+    return days[day];
+  }
+  function getHour(time) {
+    // get hour on format 12:00am 4:00pm
+    // set minutes to 00
+    // 12:18am --> 12:00am
+    const indexOfColon = time.indexOf(':');
+    return `${time.substring(0, indexOfColon)}:00${time.match(/(am)|(pm)/)[0]}`;
+  }
+  async function getData(subr) {
+    const response = await fetch(`https://www.reddit.com/r/${subr}/top.json?t=year&limit=100`);
+    const result = await response.json();
+    // get only fields that we are interested
+    const filteredFields = result.data.children.map((el) => (
+      {
+        author: el.data.author,
+        permalink: el.data.permalink,
+        title: el.data.title,
+        score: el.data.score,
+        num_comments: el.data.num_comments,
+        time: getTime(el.data.created_utc),
+        day: getDay(el.data.created_utc),
+      }
+    ));
+    // the structure of data that are going to be displayed on heat map
+    const datagrid = {
+      Sunday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Monday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Tuesday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Wednesday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Thursday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Friday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+      Saturday: {
+        '12:00am': [],
+        '1:00am': [],
+        '2:00am': [],
+        '3:00am': [],
+        '4:00am': [],
+        '5:00am': [],
+        '6:00am': [],
+        '7:00am': [],
+        '8:00am': [],
+        '9:00am': [],
+        '10:00am': [],
+        '11:00am': [],
+        '12:00pm': [],
+        '1:00pm': [],
+        '2:00pm': [],
+        '3:00pm': [],
+        '4:00pm': [],
+        '5:00pm': [],
+        '6:00pm': [],
+        '7:00pm': [],
+        '8:00pm': [],
+        '9:00pm': [],
+        '10:00pm': [],
+        '11:00pm': [],
+      },
+    };
+    days.forEach((day) => {
+      filteredFields.map((el) => datagrid[day][getHour(el.time)].push(el));
+    });
+    // set data of heat map
+    setSearchData(datagrid);
+  }
+  // get data
+  getData(params.subreddit);
 
   function handleSubmit(event) {
     event.preventDefault();
     history.push(`/search/${subreddit}`);
-  }
-  function handleChange(event) {
-    setSubreddit(event.target.value);
+    getData(params.subreddit);
   }
 
   useEffect(() => {
@@ -67,6 +312,10 @@ const SearchForm = () => {
       </Form>
     </div>
   );
+};
+
+SearchForm.propTypes = {
+  setSearchData: PropType.func.isRequired,
 };
 
 export default SearchForm;
--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -1,24 +1,15 @@
 /* eslint-disable linebreak-style */
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import * as S from '../components/Styled.components';
 
 import SearchForm from '../components/SearchForm';
 
 const Search = () => {
-  // const [searchData, setSearchData] = useState({});
-
-  // async function getData(subreddit) {
-  //   const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100`);
-  //   console.log(response);
-  // }
-  // // get data when the page renders
-  useEffect(() => {
-    // getData();
-  }, []);
-
+  const [searchData, setSearchData] = useState({});
+  console.table(searchData);
   return (
     <S.PageWrapper>
-      <SearchForm />
+      <SearchForm setSearchData={setSearchData} />
     </S.PageWrapper>
   );
 };
